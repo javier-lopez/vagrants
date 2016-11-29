ddfa6c6f8d7be1f21cd3db30eb9ea426c07c3af4
Javier López <m@javier.io>
2016-11-29 00:44:42 -0600

    add ubuntu-14.04-mongodb-3.0

---
+++ ubuntu-14.04-mongodb-3.0/provision-mongodb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#https://docs.mongodb.org/v3.0/tutorial/install-mongodb-on-ubuntu/
+locale-gen UTF-8
+apt-get update && apt-get install squid-deb-proxy-client #accelerate download process if there is a local apt-proxy nearby
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get update
+apt-get install -y mongodb-org=3.0.10 mongodb-org-server=3.0.10 mongodb-org-shell=3.0.10 mongodb-org-mongos=3.0.10 mongodb-org-tools=3.0.10
+echo "mongodb-org hold"        | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold"  | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold"  | sudo dpkg --set-selections
+service mongod restart
