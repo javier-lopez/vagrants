03aa5d6fac149e52a97885a074fa5755670604ea
Javier López <m@javier.io>
2018-01-17 21:30:32 -0600

    minos-core-16.04-docker: add apt-transport-https dependency

--- minos-core-16.04-docker/provision/01-install-docker.sh
+++ minos-core-16.04-docker/provision/01-install-docker.sh
@@ -3,7 +3,7 @@
 set -xe
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-get install -y software-properties-common
+sudo apt-get install -y software-properties-common apt-transport-https
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
