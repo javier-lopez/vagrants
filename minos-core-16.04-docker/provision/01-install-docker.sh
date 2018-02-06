500b621c5c6eabbee3cd57844cc57bdb2adad679
Javier López <m@javier.io>
2018-02-05 23:12:18 -0600

    minos-core-16.04-docker: rm provision/00-apt-update.sh

--- minos-core-16.04-docker/provision/01-install-docker.sh
+++ minos-core-16.04-docker/provision/01-install-docker.sh
@@ -5,7 +5,7 @@ if ! command -v "docker" >/dev/null 2>&1; then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo apt-get install -y software-properties-common apt-transport-https
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update
+    sudo apt-get -y update
     sudo apt-get install -y docker-ce
 fi
 
